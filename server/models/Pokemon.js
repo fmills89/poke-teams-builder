759817614b87b0b74c08e01725715b6dfbf06ffa
coevpen <koinuevins@gmail.com>
2022-04-05 20:59:31 -0500

    add pokemon and team models

--- server/models/Pokemon.js
+++ server/models/Pokemon.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const Pokemon = require('./Pokemon');
 
 const teamSchema = new Schema({
-    pokemon: [Pokemon],
-    validate: [arrayLimit, 'Must not exceed 6 pokemon']
+    pokemon:{
+        type: String,
+        required: true
+    }
 });
 
-function arrayLimit(val) {
-    return val.length <= 6;
-}
 
 const Team = mongoose.model('Team', teamSchema);
 

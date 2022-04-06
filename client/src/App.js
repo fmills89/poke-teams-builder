549e67f38933e8005a7d9aa0cc88185444613214
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-04-05 19:15:27 -0500

    develop push

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import NoMatch from './pages/NoMatch';
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 function SignUp() {
     return (
         <div>
+            <>
+            <Header />
+            <Nav />
+            </>
+
             <div>
                 <Link to='/Login'>Go to Login</Link>
             </div>

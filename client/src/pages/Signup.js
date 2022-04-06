da3a302e1f1ed28f30b9120e79938e5612db75ae
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-04-05 20:54:01 -0500

    pulling from develop

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import profOak from '../assets/professor-oak-image.png'
 
 function SignUp() {
     return (
@@ -10,14 +11,27 @@ function SignUp() {
             <Header />
             <Nav />
             </>
-
-            <div>
-                <Link to='/Login'>Go to Login</Link>
+                <div class='min-h-screen text-gray-800 antialiased px-4 py-6 flex flex-col justify-center sm:py-12'>
+                <div>
+                    <img className='' src={profOak} alt='profressor oak' />
+                </div>
+                    <div class='relative py-3 sm:max-w-xl mx-auto text-center'>
+                        <span class='text-2xl font-light'>Ready to build some teams?!</span>
+                    </div>
+                    <div className='relative mt-8 bg-white shadow-md sm:rounded-lg text-left'>
+                        <div className='h-2 bg-red-400 rounded-t-md'></div>
+                        <div className='py-6 px-8'>
+                            <label className='block font-semibold'>Username</label>
+                            <input type='text' placeholder='Username' className='border-red-300 w-full h-5 px-3 py-5 mt-2 rounded-md'></input>
+                            <label className='block mt-3 font-semibold'>Password</label>
+                            <input type='password' placeholder='Password' className='border-red-300 w-full h-5 px-3 py-5 my-2 rounded-md'></input>
+                            <div className='flex justify-between items-baseline'>
+                                <button className='mt-4 bg-red-500 text-white py-2 px-6 rounded-lg'>Sign-up</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <h2>Sign-Up</h2>
-            </div>
-        </div>
     );
 };
 

aaa9cda6ad8e35bfa6a6edfa00bc6bc763d579c8
coevpen <koinuevins@gmail.com>
2022-04-07 19:47:25 -0500

    update addTeam functions and mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -47,7 +47,7 @@ const typeDefs = gql`
 	type Mutation {
 		addUser(username: String!, password: String!): Auth
 		login(username: String!, password: String!): Auth
-		addTeam(pokemon: [PokemonInput]!): Team
+		addTeam(): Team
 		addToTeam(teamId: ID!, pokemon: PokemonInput!): Team
 		removeFromTeam(teamId: ID!, pokemonId: ID!): Team
 		removeTeam(_id: ID!): Team
